97ccfe2061162f40fe583ef7b467b68934a0e185
askyclear <askyclear2@gmail.com>
2022-02-11 21:00:00 +0900

    [문제풀이]프로그래머스 코딩테스트 연습
    - 코딩테스트 이중우선순위큐

---
+++ src/com/programmers/algorigm/DualPriortyQueue.java
@@ -0,0 +1,33 @@
+package com.programmers.algorigm;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class DualPriortyQueue {
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> ascQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> descQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        for(String operation : operations) {
+            String[] splitOperation = operation.split(" ");
+            Integer value = Integer.parseInt(splitOperation[1]);
+            if (splitOperation[0].equals("I")) {
+                ascQueue.add(value);
+                descQueue.add(value);
+            } else if (!ascQueue.isEmpty()){
+                if (value > 0) {
+                    int removeValue = descQueue.poll();
+                    ascQueue.remove(removeValue);
+                } else {
+                    int removeValue = ascQueue.poll();
+                    descQueue.remove(removeValue);
+                }
+            }
+        }
+
+        if (ascQueue.isEmpty()) {
+            return new int[]{0, 0};
+        } else {
+            return new int[]{ascQueue.poll(), descQueue.poll()};
+        }
+    }
+}
