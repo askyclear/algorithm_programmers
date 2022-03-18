faed26bc197e17ee7a3822d843f9cece50f16ea3
askyclear <askyclear2@gmail.com>
2022-03-18 20:00:00 +0900

    [문제풀이]프로그래머스 코딩테스트 연습
    -- 단어변환

---
+++ src/com/programmers/algorigm/WordChange.java
@@ -0,0 +1,40 @@
+package com.programmers.algorigm;
+
+public class WordChange {
+    private static boolean[] visited;
+    
+    public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+
+        return dfs(begin, target, words, 0);
+    }
+
+    public int dfs(String begin, String target, String[] words, int cnt) {
+        if (begin.equals(target)) {
+            return cnt;
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            int k = 0;
+            for (int j = 0; j < begin.length(); j++) {
+                if (begin.charAt(j) == words[i].charAt(j)) {
+                    k++;
+                }
+            }
+
+            if (k == begin.length() - 1) {
+                visited[i] = true;
+                answer = dfs(words[i], target, words, cnt + 1);
+                visited[i] = false;
+            }
+        }
+
+        return answer;
+    }
+}
