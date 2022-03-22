0b8f6c221e6484ec40daba30f4d4e70f3732342a
askyclear <askyclear2@gmail.com>
2022-03-22 19:39:19 +0900

    [프로그래머스] Dev-Matchin:웹 백엔드 개발
    - 로또의 최고 순위와 최저 순위

---
+++ src/com/programmers/algorigm/LottoMaxMinRank.java
@@ -0,0 +1,32 @@
+package com.programmers.algorigm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LottoMaxMinRank {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        Set<Integer> winNumsSet = new HashSet<>();
+
+        for (int winNum : win_nums) {
+            winNumsSet.add(winNum);
+        }
+
+        int correctCount = 0;
+        int unknownCount = 0;
+
+        for(int lotto : lottos) {
+            if (winNumsSet.contains(lotto)) {
+                correctCount++;
+            }
+            if (lotto == 0) {
+                unknownCount++;
+            }
+        }
+        
+        answer[0] = 7 - Math.max((correctCount + unknownCount), 1);
+        answer[1] = 7 - Math.max(correctCount, 1);
+
+        return answer;
+    }
+}
