785434464b3b23d8c4e9ce547e14242eb7c4cde7
askyclear <askyclear2@gmail.com>
2022-04-18 21:30:59 +0900

    [프로그래머스] 소수 만들기

--- src/com/programmers/algorigm/Main.java
+++ src/com/programmers/algorigm/Main.java
@@ -3,7 +3,7 @@ package com.programmers.algorigm;
 
 public class Main {
     public static void main(String[] args) {
-        int[] prices = {1, 2, 3,2,3};
+        int[] prices = {1, 2, 3, 2, 3};
         int[] discounts = {30, 20};
         int[] priorties = {1, 1, 9, 1, 1, 1};
         int location = 0;
@@ -18,57 +18,57 @@ public class Main {
         StockPrice stockPrice = new StockPrice();
         stockPrice.solution(prices);
 
-        int[] scoville = {1,2,3,9,10,12};
+        int[] scoville = {1, 2, 3, 9, 10, 12};
         int K = 7;
         Spicy spicy = new Spicy();
         spicy.solution(scoville, K);
 
         DiskController diskController = new DiskController();
-        int[][] jobs = {{0,3}, {1,9}, {2,6}};
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
         diskController.solution(jobs);
 
         DualPriortyQueue dualPriortyQueue = new DualPriortyQueue();
-        String[] operations = {"I 7","I 5","I -5","D -1"};
+        String[] operations = {"I 7", "I 5", "I -5", "D -1"};
 
         dualPriortyQueue.solution(operations);
         KthNumber kthNumber = new KthNumber();
-        int[] array = {1,5,2,6,3,7,4};
-        int[][]commands = {{2,5,3}, {4,4,1}, {1,7,3}};
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
         kthNumber.solution(array, commands);
 
         BigNumber bigNumber = new BigNumber();
-        bigNumber.solution(new int[]{0,0,0,0,0,0});
+        bigNumber.solution(new int[]{0, 0, 0, 0, 0, 0});
         HIndex hIndex = new HIndex();
-        hIndex.solution(new int[]{5,5,5,5});
+        hIndex.solution(new int[]{5, 5, 5, 5});
         MockExam mockExam = new MockExam();
-        mockExam.solution(new int[]{1,2,3,4,5});
-        mockExam.solution(new int[]{1,3,2,4,2});
+        mockExam.solution(new int[]{1, 2, 3, 4, 5});
+        mockExam.solution(new int[]{1, 3, 2, 4, 2});
 
         FindPrimeNumber findDecimals = new FindPrimeNumber();
         findDecimals.solution("17");
         Carpet carpet = new Carpet();
-        carpet.solution(10,2);
-        carpet.solution(8,1);
-        carpet.solution(24,24);
+        carpet.solution(10, 2);
+        carpet.solution(8, 1);
+        carpet.solution(24, 24);
 
         MakeBigNumber makeBigNumber = new MakeBigNumber();
         System.out.println(makeBigNumber.solution("4177252841", 4));
 
         WayToSchool wayToSchool = new WayToSchool();
-        wayToSchool.solution(4,3,new int[][]{{2,2}});
+        wayToSchool.solution(4, 3, new int[][]{{2, 2}});
         Thievery thievery = new Thievery();
-        thievery.solution(new int[]{8,1,1,8});
+        thievery.solution(new int[]{8, 1, 1, 8});
         TargetNumber targetNumber = new TargetNumber();
-        targetNumber.solution(new int[]{4,1,2,1}, 4);
+        targetNumber.solution(new int[]{4, 1, 2, 1}, 4);
 
         Immigration immigration = new Immigration();
-        System.out.println(immigration.solution(3, new int[] {1,2,3}));
+        System.out.println(immigration.solution(3, new int[]{1, 2, 3}));
 
         SteppingStone steppingStone = new SteppingStone();
-        steppingStone.solution(25, new int[]{2,14,11,21,17}, 2);
+        steppingStone.solution(25, new int[]{2, 14, 11, 21, 17}, 2);
 
         KakaoBlindRecruitment1 kakaoBlindRecruitment1 = new KakaoBlindRecruitment1();
-        kakaoBlindRecruitment1.solution(new String[]{"muzi","frodo", "apeach", "neo"}, new String[]{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, 2);
+        kakaoBlindRecruitment1.solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}, 2);
 
         LottoMaxMinRank lottoMaxMinRank = new LottoMaxMinRank();
         lottoMaxMinRank.solution(new int[]{44, 1, 0, 0, 31, 25}, new int[]{31, 10, 45, 1, 6, 19});
@@ -82,6 +82,50 @@ public class Main {
         newId.solution("abcdefghijklmn.p");
         KakaoBlindRecruitment2021NumbereStr numbereStr = new KakaoBlindRecruitment2021NumbereStr();
         numbereStr.solution("one4seveneight");
+
+        Kakao2020Keypad kakao2020Keypad = new Kakao2020Keypad();
+        kakao2020Keypad.solution(new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5}, "right");
+        kakao2020Keypad.solution(new int[]{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}, "left");
+        kakao2020Keypad.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, "right");
+        Kakao2019Crain crain = new Kakao2019Crain();
+        crain.solution(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}}, new int[]{1, 5, 3, 5, 1, 2, 1, 4});
+        crain.solution2(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}}, new int[]{1, 5, 3, 5, 1, 2, 1, 4});
+        KakoBlindRecruitmentStringCompression compression = new KakoBlindRecruitmentStringCompression();
+        compression.solution("aabbaccc");
+        compression.solution("ababcdcdababcdcd");
+        compression.solution("abcabcdede");
+        compression.solution("abcabcabcabcdededededede");
+        compression.solution("xababcdcdababcdcd");
+
+        MenuRenewal renewal = new MenuRenewal();
+        renewal.solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2,3,4});
+        renewal.solution(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2,3,5});
+        renewal.solution(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2,3,4});
+
+        FailRate failRate = new FailRate();
+        failRate.solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3});
+        failRate.solution(4, new int[]{1,1,1,1,1});
+
+        BracketConversion bracketConversion = new BracketConversion();
+        System.out.println(bracketConversion.solution("()))((()"));
+        System.out.println(bracketConversion.solution("(()())()"));
+        System.out.println(bracketConversion.solution("()"));
+        System.out.println(bracketConversion.solution(")("));
+        System.out.println(bracketConversion.solution("((((()))))"));
+        System.out.println(bracketConversion.solution(")))))((((("));
+
+        NewsClustering newsClustering = new NewsClustering();
+        newsClustering.solution("ab", "cd");
+        KakaoSpecialConverter kakaoSpecialConverter = new KakaoSpecialConverter();
+        kakaoSpecialConverter.solution("AAaAA");
+        MaximizeFormulas maximizeFormulas = new MaximizeFormulas();
+        System.out.println(maximizeFormulas.solution("100-200*300-500+20"));
+        System.out.println(maximizeFormulas.solution("50*6-3*2"));
+        PrimeNumberCreator primeNumberCreator = new PrimeNumberCreator();
+        System.out.println(primeNumberCreator.solution(new int[]{1,2,3,4}));
+        System.out.println(primeNumberCreator.solution(new int[]{1,2,7,6,4}));
+        System.out.println(primeNumberCreator.solution(new int[]{1,2,3}));
+        System.out.println(primeNumberCreator.solution(new int[]{1,2,4}));
     }
 
 }
--- src/com/programmers/algorigm/MaximizeFormulas.java
+++ src/com/programmers/algorigm/MaximizeFormulas.java
@@ -1,28 +1,19 @@
 package com.programmers.algorigm;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.List;
 
 public class MaximizeFormulas {
     private char plus = "+".charAt(0);
     private char minus = "-".charAt(0);
     private char mul = "*".charAt(0);
+    private char[] operand = new char[]{plus, minus, mul};
+    private boolean[] isVisited = new boolean[]{false,false,false};
 
     public long solution(String expression) {
-        long case1 = Math.abs(Long.parseLong(calculate2(mul, calculate2(minus, calculate2(plus, expression)))));
-        long case2 = Math.abs(Long.parseLong(calculate2(minus, calculate2(mul, calculate2(plus, expression)))));
-        long case3 = Math.abs(Long.parseLong(calculate2(mul, calculate2(plus, calculate2(minus, expression)))));
-        long case4 = Math.abs(Long.parseLong(calculate2(plus, calculate2(mul, calculate2(minus, expression)))));
-        long case5 = Math.abs(Long.parseLong(calculate2(minus, calculate2(plus, calculate2(mul, expression)))));
-        long case6 = Math.abs(Long.parseLong(calculate2(plus, calculate2(minus, calculate2(mul, expression)))));
-
-        return Math.max(Math.max(Math.max(Math.max(Math.max(case1, case2), case3),case4), case5), case6);
-    }
-
-    private String calculate2(char exp, String expression) {
-        Queue<Long> numberStack = new LinkedList<>();
-        Queue<String> operandStack = new LinkedList<>();
+        List<Long> numberStack = new ArrayList<>();
+        List<String> operandStack = new ArrayList<>();
         String number = "";
         int lastOperandIndex = 0;
         for (int i = 0; i < expression.length(); i++) {
@@ -30,30 +21,60 @@ public class MaximizeFormulas {
             if (Character.isDigit(charExpression)) {
                 number += Character.toString(charExpression);
             } else {
-                numberStack.add(Long.parseLong(number.isEmpty() ? "0" : number));
+                numberStack.add(Long.parseLong(number));
                 operandStack.add(Character.toString(charExpression));
                 lastOperandIndex = i + 1;
                 number = "";
             }
         }
+
         numberStack.add(Long.parseLong(expression.substring(lastOperandIndex)));
-        String answer = "";
-        while (!numberStack.isEmpty()) {
-            long preDigit = numberStack.poll();
-            String operand = operandStack.isEmpty() ? "" : operandStack.poll();
-            if (!operand.isEmpty() && operand.charAt(0) == exp) {
-                Queue<Long> tempQueue = new LinkedList<>();
-                tempQueue.add(calcurateExp(exp, preDigit, numberStack.poll()));
-                tempQueue.addAll(numberStack);
-                numberStack.clear();
-                numberStack.addAll(tempQueue);
-            } else {
-                answer = answer + (preDigit == 0L ? "" : preDigit + operand);
+
+
+        return dfs(0, new ArrayList<>(), numberStack, operandStack);
+    }
+
+    private long dfs(int depth, List<String> priorityOperand,List<Long> numberList, List<String> operandList) {
+        long answer = 0;
+        if (depth == 3) {
+            return calculate(priorityOperand, numberList, operandList);
+        } else {
+            for (int i = 0; i < operand.length; i++) {
+                if (!isVisited[i]) {
+                    List<String> tempOperand = new ArrayList<>(priorityOperand);
+                    tempOperand.add(Character.toString(operand[i]));
+                    isVisited[i] = true;
+                    answer = Math.max(answer, dfs(depth + 1, tempOperand, numberList, operandList));
+                    isVisited[i] = false;
+                }
             }
         }
+
         return answer;
     }
 
+    private long calculate(List<String> priorityOperand, List<Long> numberList, List<String> operandList) {
+        List<Long> copyNumberList = new ArrayList<>(numberList);
+        List<String> copyOperandList = new ArrayList<>(operandList);
+        for (int i = 0; i < priorityOperand.size(); i++) {
+            String curOperand = priorityOperand.get(i);
+            for (int j = 0; j < copyOperandList.size(); j++) {
+                if (!copyOperandList.get(j).equals(curOperand)) {
+                    continue;
+                }
+                long preNum = copyNumberList.get(j);
+                long nextNum = copyNumberList.get(j + 1);
+                long tempNumb = calcurateExp(curOperand.charAt(0), preNum, nextNum);
+
+                copyNumberList.remove(j + 1);
+                copyNumberList.remove(j);
+                copyNumberList.add(j, tempNumb);
+                copyOperandList.remove(j);
+                j--;
+            }
+        }
+        return Math.abs(copyNumberList.get(0));
+    }
 
     private long calcurateExp(char exp, long a, long b) {
         if (Character.toString(exp).equals("*")) {
