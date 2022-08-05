0666f31dd3ec5288fa04ade492ad9c34a6b295d2
askyclear <askyclear2@gmail.com>
2022-08-05 19:25:55 +0900

    [프로그래머스] 2021 KAKAO BLIND RECRUITMENT -- 순위검색

--- src/com/programmers/algorigm/year2022/month05/RankSearch.java
+++ src/com/programmers/algorigm/year2022/month05/RankSearch.java
@@ -1,76 +1,63 @@
 package com.programmers.algorigm.year2022.month05;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class RankSearch {
-    public int[] solution2(String[] info, String[] query) {
-        int[] answer = new int[query.length];
-        for (int i = 0; i < query.length; i++) {
-//            answer[i] = filtered(query[i].split("and"), info);
-        }
-
-
-        return answer;
-    }
-
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        List<Info> infoList = new ArrayList<>();
+        Map<String, List<Integer>> peopleMap = new HashMap<>();
 
         for (String peopleInfo : info) {
             String[] infos = peopleInfo.split(" ");
-
-            infoList.add(new Info(infos[0], infos[1], infos[2], infos[3], Integer.parseInt(infos[4])));
+            String[] langs = {infos[0], "-"};
+            String[] jobGroups = {infos[1], "-"};
+            String[] carreas = {infos[2], "-"};
+            String[] favorutis = {infos[3], "-"};
+            Integer score = Integer.parseInt(infos[4]);
+            for (String lang : langs) {
+                for(String jobGroup : jobGroups) {
+                    for (String carre : carreas) {
+                        for(String favority : favorutis) {
+                            String key = lang + jobGroup + carre + favority;
+                            List<Integer> scores = peopleMap.getOrDefault(lang + jobGroup + carre + favority, new ArrayList<>());
+                            scores.add(score);
+                            peopleMap.put(key, scores);
+                        }
+                    }
+                }
+            }
         }
-
         for (int i = 0; i < query.length; i++) {
-            answer[i] = filtered(query[i].split(" and "), infoList);
-        }
+            String[] quey = query[i].replaceAll(" and ", "").split(" ");
+            Integer compareScore = Integer.parseInt(quey[1]);
+            if(peopleMap.containsKey(quey[0])) {
+                List<Integer> scores = peopleMap.get(quey[0]);
+                Collections.sort(scores);
 
+                answer[i] = getPeopleCount(scores, compareScore);
+            }
+        }
 
         return answer;
     }
-    private class Info{
-        private String lang;
-        private String jobGroup;
-        private String career;
-        private String favorite;
-        private Integer score;
 
-        public Info(String lang, String jobGroup, String career, String favorite, Integer score) {
-            this.lang = lang;
-            this.jobGroup = jobGroup;
-            this.career = career;
-            this.score = score;
-            this.favorite = favorite;
+    private int getPeopleCount(List<Integer> scores, Integer compareScore) {
+        int min = 0;
+        int max = scores.size() - 1;
+        int mid;
+        if (scores.size() == 1) {
+            return scores.get(0) >= compareScore ? 1 : 0;
         }
-    }
+        while (min <= max) {
+            mid = (min + max) / 2;
+            if (scores.get(mid) < compareScore) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
 
-    public int filtered(String[] query, List<Info> infoList) {
-        return infoList.stream().filter((info) -> {
-            if(query[0].equals("-")) {
-                return true;
-            }
-
-            return query[0].equals(info.lang);
-        }).filter((info) -> {
-            if(query[1].equals("-")) {
-                return true;
-            }
-            return query[1].equals(info.jobGroup);
-        }).filter((info) -> {
-            if(query[2].equals("-")) {
-                return true;
-            }
-            return query[2].equals(info.career);
-        }).filter((info) -> {
-            String[] lastQuery = query[3].split(" ");
-            if(lastQuery[0].equals(info.favorite) || lastQuery[0].equals("-")) {
-                return info.score >= Integer.parseInt(lastQuery[1]);
-            }
-            return false;
-        }).collect(Collectors.toList()).size();
+        return scores.size() - min;
     }
 }
