b68f85283cc39ad0e46ce34ffd814e16585e0ddd
askyclear <askyclear2@gmail.com>
2022-08-05 18:52:29 +0900

    [프로그래머스] 2021 KAKAO BLIND RECRUITMENT -- 순위검색
    -- 다른 방법으로 변경

--- src/com/programmers/algorigm/year2022/month05/RankSearch.java
+++ src/com/programmers/algorigm/year2022/month05/RankSearch.java
@@ -1,40 +1,76 @@
 package com.programmers.algorigm.year2022.month05;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RankSearch {
     public int[] solution2(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        for (int i = 0 ; i < query.length; i++) {
-            answer[i] = filtered(query[i].split("and"), info, 0);
+        for (int i = 0; i < query.length; i++) {
+//            answer[i] = filtered(query[i].split("and"), info);
         }
 
 
         return answer;
     }
+
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        for (int i = 0 ; i < query.length; i++) {
-            answer[i] = filtered(query[i].split("and"), info, 0);
+        List<Info> infoList = new ArrayList<>();
+
+        for (String peopleInfo : info) {
+            String[] infos = peopleInfo.split(" ");
+
+            infoList.add(new Info(infos[0], infos[1], infos[2], infos[3], Integer.parseInt(infos[4])));
+        }
+
+        for (int i = 0; i < query.length; i++) {
+            answer[i] = filtered(query[i].split(" and "), infoList);
         }
 
 
         return answer;
     }
+    private class Info{
+        private String lang;
+        private String jobGroup;
+        private String career;
+        private String favorite;
+        private Integer score;
 
-    public int filtered(String[] query, String[] info, int depth) {
-        if (info.length ==0) {
-            return 0;
+        public Info(String lang, String jobGroup, String career, String favorite, Integer score) {
+            this.lang = lang;
+            this.jobGroup = jobGroup;
+            this.career = career;
+            this.score = score;
+            this.favorite = favorite;
         }
-        String parsedQuery = depth >= 3 ? query[3].trim().split(" ")[depth-3] : query[depth].trim();
-        if (depth == 4) {
-            return Arrays.stream(info)
-                    .filter(s -> Integer.parseInt(s.split(" ")[depth]) >=Integer.parseInt(parsedQuery)
-                    ).toArray().length;
-        }
+    }
 
-        return filtered(query, Arrays.stream(info)
-                .filter(s -> (parsedQuery.equals("-") || parsedQuery.equals(s.split(" ")[depth]))
-                ).toArray(String[]::new), depth + 1);
+    public int filtered(String[] query, List<Info> infoList) {
+        return infoList.stream().filter((info) -> {
+            if(query[0].equals("-")) {
+                return true;
+            }
+
+            return query[0].equals(info.lang);
+        }).filter((info) -> {
+            if(query[1].equals("-")) {
+                return true;
+            }
+            return query[1].equals(info.jobGroup);
+        }).filter((info) -> {
+            if(query[2].equals("-")) {
+                return true;
+            }
+            return query[2].equals(info.career);
+        }).filter((info) -> {
+            String[] lastQuery = query[3].split(" ");
+            if(lastQuery[0].equals(info.favorite) || lastQuery[0].equals("-")) {
+                return info.score >= Integer.parseInt(lastQuery[1]);
+            }
+            return false;
+        }).collect(Collectors.toList()).size();
     }
 }
